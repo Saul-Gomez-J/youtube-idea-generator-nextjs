91868a8f864ea71f2429ff217e6a70f2a71dff1e
saul <gomezjimenezsaul@gmail.com>
2024-11-25 23:26:47 +0100

    one trial6

--- components/VideoList.tsx
+++ components/VideoList.tsx
@@ -9,7 +9,6 @@ import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { formatCount } from "@/lib/utils";
 import { Loader2, TvMinimal, X } from "lucide-react";
-import { addUserIfNotExists } from "@/server/mutations"
 
 // Importamos las acciones del servidor como funciones
 import { scrapeVideos, removeVideoForUser } from "@/server/youtube-actions";
